2a692ae0199e054a36a74c6b10122d0e75810a67
jaiakashkrishna <106476405+jaiakashkrishna@users.noreply.github.com>
2024-06-20 22:00:36 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,2 +1,14 @@
-a=10
-print(++a)
+import unittest
+
+def add(a, b):
+    return a + b
+
+class TestMain(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(0, 0), 0)
+
+if __name__ == "__main__":
+    unittest.main()
+
