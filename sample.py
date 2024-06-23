2b0fe55fdc32b56e73b86561184c4bb0c83261a2
jaiakashkrishna <106476405+jaiakashkrishna@users.noreply.github.com>
2024-06-23 14:03:37 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,16 +1,4 @@
-import unittest
-
-def get_details():
-    name = "John Doe"
-    age = 30
-    return f"My name is {name} and I am {age} years old."
-
-class TestMain(unittest.TestCase):
-    def test_get_details(self):
-        self.assertEqual(get_details(), "My name is John Doe and I am 30 years old.")
-
-if __name__ == "__main__":
-    print(get_details())
-    unittest.main()
-
+a=5
+for i in range(a):
+    print("*")
 
